a83142beec23d62e2d8e40b9d9e6d178413674d1
Clara Tu <claratu@berkeley.edu>
2022-10-31 14:54:41 -0700

    added modals

--- src/Card.js
+++ src/Card.js
@@ -5,6 +5,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Modal from './Modal'
 
 const bull = (
   <Box
@@ -20,22 +21,20 @@ export default function BasicCard() {
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
+          PlexSpot
         </Typography>
         <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
+          Study Spot Name
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
+          IMG of spot
         </Typography>
         <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+          tags
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <Modal>Modal</Modal>
       </CardActions>
     </Card>
   );
