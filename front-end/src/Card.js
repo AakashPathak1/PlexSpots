3507143f4a5880bd4b7fa6e884b6ec12207b5b96
Aakash Pathak <apathak1406@gmail.com>
2022-11-03 14:02:12 -0700

    ivan help me

---
+++ front-end/src/Card.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import Modal from './Modal';
+
+export default function MultiActionAreaCard() {
+  //const [open, setOpen] = useState(false);
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea onClick={() => Modal.setOpen(true)}>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://www.constructionspecifier.com/wp-content/uploads/2018/11/Opener-7.jpg"
+          alt="dungeon"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            The Dungeon
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Hours: 8am - 6am
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Modal>Modal</Modal>
+      </CardActions>
+    </Card>
+  );
+}
---
+++ front-end/src/index.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
+import App from './App';
+
+
+  
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
+  </React.StrictMode>
+);
+
+  
+  // ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
