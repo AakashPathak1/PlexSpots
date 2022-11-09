56e8031fcf106be2e52f594ee64a9920d938bf50
Aakash Pathak <apathak1406@gmail.com>
2022-11-09 14:38:10 -0800

    Revert "Card"

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
-import { StyledEngineProvider } from '@mui/material/styles';
-import App from './App';
+import './index.css';
 
 
   
-ReactDOM.createRoot(document.querySelector("#root")).render(
-  <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <App />
-    </StyledEngineProvider>
-  </React.StrictMode>
-);
+class Intro extends React.Component {
+  render() {
+    return (
+      <div>
+        Our Website will be here :)
+      </div>
+    );
+  }
+}
+
 
   
   // ========================================
-
+  
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
-
+root.render(<Intro />);
+  
